d37baeb184895a89c3f4947694b37d92f182a83a
misbah0047 <misbah57990@gmail.com>
2026-02-25 10:55:04 +0530

    learning js fundamentals

---
+++ part-1/datatypes.js
@@ -0,0 +1,24 @@
+let username = "Misbah";
+// let username2 ='sana'
+
+console.log(username);
+let score = 100;
+let score2 = 100.34;
+console.log(score);
+console.log(score2);
+let isLoggedIn = false;
+let isValiduser = false;
+console.log(isLoggedIn);
+let isUndefined;
+console.log(isUndefined);
+let temp = null;
+console.log(temp);
+let obj = {
+  username: "Misbah",
+  isActive: false,
+};
+
+console.log(obj);
+console.log(obj.username);
+let arrryy = ["Misbah", true, 100, 11.56];
+console.log(arrryy[2]);
---
+++ part-1/printing.js
@@ -0,0 +1,5 @@
+console.log('Hello There')
+console.log('I am here!')
+
+console.warn('This is a warning msg')
+console.table({city:'bijapur'})
