3d0007973940b0c792565653c6190c7244b884f3
misbah0047 <misbah57990@gmail.com>
2026-02-26 09:53:28 +0530

    add logical & operation file into codebase

---
+++ part-1/logical.js
@@ -0,0 +1,15 @@
+// && => and
+// || => or
+//&& => and
+// ! => reverse
+
+
+
+
+
+let isLoggedIn = true;
+let ispaid =false;
+console.log(isLoggedIn && ispaid)
+let isEmailuser = true;
+let isGoogleuser = false;
+console.log(isEmailuser || isGoogleuser )
